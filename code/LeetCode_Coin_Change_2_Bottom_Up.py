1156b2c6c50e9cb9c9d47b20f0e9f3012bdecc47
Shrinivasan Patnaikuni (P.S.R.Patnaik) <psrpatnaik@gmail.com>
2021-08-01 20:40:20 +0530

    Create LeetCode_Coin_Change_2_Bottom_Up.py

---
+++ code/LeetCode_Coin_Change_2_Bottom_Up.py
@@ -0,0 +1,13 @@
+class Solution:
+    def change(self, amount: int, coins: List[int]) -> int:
+        coins = sorted(coins,reverse=True)
+        dp = [[0 for i in range (amount+1) ]for j in range (len (coins)+1)]
+        dp[0][0] = 1
+        for i in range(1, len(coins)+1):
+            dp[i][0] = 1
+            for j in range(1, (amount+1)):
+                dp[i][j] = dp[i-1][j] +  (dp[i][j - coins[i-1]] if j - coins[i-1] >= 0  else 0)
+        return dp[len(coins)][amount]
+     
+        
+        
